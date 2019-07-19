2117c4b125b231206b2bd72310b9290642cf0485
renan <renannunes400@gmail.com>
2019-07-19 12:44:38 -0300

    Update No.h
    Header de Nós, contém a classe Nó e a Classe Vértice. Em termos de Teoria essas palavras são sinônimos e representam a mesma coisa. Para o código a classe No possui apenas o peso e o id. A classe Vértice guarda o ponteiro para o próximo Nó e futuramente guardará a lista de adjacência de arestas.

---
+++ código/No.h
@@ -0,0 +1,70 @@
+#ifdef NO_H_INCLUDED
+#define NO_H_INCLUDED
+
+class No{
+
+private:
+    int id;
+    int grauEntrada;
+    int grauSaida;
+    int peso;
+
+public:
+    /**
+    * Construtor Vazio
+    **/
+    No();
+    /**
+    * Construtor de um No
+    * @param id é o indice de um Vértice
+    * @param pesoNo é o peso de um Vértice
+    * @param grauDeEntrada representa o número de Arestas que vão para um Nó
+    * @param grauDeSaida representa o número de Arestas que partem de um Nó
+    **/
+    No(int id, int pesoNo, int grauDeEntrada,int grauDeSaida);
+    /** 
+    * Destrutor Vazio 
+    **/
+    ~No(){};
+
+    /* Getters e Setter */
+    int getId() {return this->id;};
+    int getGrauEntrada(){return this->grauEntrada;};
+    int getGrauSaida(){return this->grauSaida;};
+    int getPeso(){return this->peso;};
+    void setId(int n){this->id = n;};
+    void setGrauEntrada(int n){this->grauEntrada = n;};
+    void setGrauSaida(int n){this->grauSaida = n;};
+    void setPeso(int n){this-peso = n;};
+
+};
+
+
+class Vertices { // Vértice é um elemento da Lista de Nós
+
+private:
+    No* v;
+    Vertices* prox;
+public:
+    /** 
+    * Construtor de Vértices 
+    **/
+    Vertices(No* n){
+        this->v = n;
+        this->prox = nullptr;
+    };
+
+    /**
+    * Destrutor de Vértices  
+    **/
+    ~Vertices(){
+        delete this->v;
+    };
+
+    /* Getters e Setters */
+    No* getNo(){return this->v;};
+    Vertices* getProx(){return this->prox;};
+    void setProx(Vertices* n){tis->prox = n;};
+};
+
+#endif //NO_H_INCLUDED
