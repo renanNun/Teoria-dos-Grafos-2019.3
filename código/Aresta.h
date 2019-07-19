f939823082df613d232fd0cc17980559bbad77c9
renan <renannunes400@gmail.com>
2019-07-19 12:55:32 -0300

    Update Aresta.h
    Header da classe Aresta Implementado

---
+++ código/Aresta.h
@@ -0,0 +1,43 @@
+#ifdef ARESTA_H_INCLUDED
+#define ARESTA_H_INCLUDED
+
+#include "No.h"
+
+class Aresta{
+
+private:
+    int peso;
+    Vertices* v;
+    Vertices* v_origem;
+    Aresta* prox;
+public:
+    /** 
+    * Construtor de Aresta
+    * @param origem é o vertice de qual a aresta se origina
+    * @param vertice é o vertice para qual a aresta se direciona
+    * @param peso é o peso do caminho de um vertice à outro 
+    **/
+    Aresta(Vertices* origem, Vertices* vertice,int peso){
+        this->v = vertice;
+        this->v_origem = origem;
+        this->peso = peso;
+        this->prox = nullptr;
+    };
+
+    /** 
+    * Destrutor de Arestas 
+    **/
+    ~Aresta(){};
+
+    /* Getters e Setters */
+    int getPeso(){return this->peso;};
+    Vertices* getOrigem(){return this->v_origem;};
+    Vertices* getVertice(){return this->v;};
+    Aresta* getProx(){retun this->prox;};
+    void setPeso(int n){this->peso = n;};
+    void setVertice(Vertices* n){this->v = n;};
+    void setProx(Aresta* n){this->prox = n;};
+
+};
+
+#endif ARESTA_H_INCLUDED
