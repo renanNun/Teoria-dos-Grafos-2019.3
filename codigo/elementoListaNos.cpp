1c7a3765a620a427b8ae3319d17c1aadf159cb13
renan <renannunes400@gmail.com>
2019-08-13 12:30:27 -0300

    Update
    Lista de Nos .cpp atualizada

--- codigo/elementoListaNos.cpp
+++ codigo/elementoListaNos.cpp
@@ -2,20 +2,45 @@
 
 using namespace std;
 
+/**
+* Construtor Vazio
+**/
 Vertice::Vertice(){
-
+    // Construtor Vazio
 }
 
+/**
+* Construtor da classe Elemento da Lista de Nos
+* @param numNos eh o numero de Nos do grafo
+* @param novon eh um novo No
+**/
 Vertice::Vertice(int numNos, No* novon){
-
+    this->n = novon;
+    this->arestas = new ListaAresta(nullptr,nullptr,0);
+    this->prox = nullptr;
 }
-Vertice::~Vertice(){
 
+/**
+* Destrutor da classe elemento da lista de Nos
+**/
+Vertice::~Vertice(){
+    delete arestas;
+    delete n;
 }
 
 /*Getters e Setters*/
-No* Vertice::getNo(){}
-ListaAresta* Vertice::getArestas(){}
-Vertice* Vertice::getProx(){}
+No* Vertice::getNo(){
+    return this->n;
+}
 
-void Vertice::setVertice(Vertice* p){}
+ListaAresta* Vertice::getArestas(){
+    return this->arestas;
+}
+
+Vertice* Vertice::getProx(){
+    return this->prox;
+}
+
+void Vertice::setVertice(Vertice* p){
+    this->prox = p;
+}
