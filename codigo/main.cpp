1ab0d765421634c57b0a4c73ae79146655a5af9c
renan <renannunes400@gmail.com>
2019-08-13 13:11:44 -0300

    Update main
    Atualizando a main e iniciando um pseudo menu, embora ainda não seja necessário ou essencial para a primeira parte do trabalho

--- codigo/main.cpp
+++ codigo/main.cpp
@@ -2,8 +2,37 @@
 
 using namespace std;
 
-int main()
+/**
+ * Trabalho de Grafos - 2019.3 Realizado pelo Grupo 2
+ * Para o funcionamento dos arquivos será usado o modelo: 
+ * g++ *.h *.cpp -o execGrupo2 && ./execGrupo2
+ *  **/
+
+
+void incluiMenu()
 {
-    cout << "Hello world!" << endl;
+    cout << endl << endl;
+    cout << "\tMenu de inicialização" << endl; 
+    cout << "\t 01) Ler novo Grafo" << endl;
+    cout << "\t 02) Imprimir Grafo" << endl;
+    cout << "\t 03) Imprimir numero de Nós e Arestas" << endl;
+    cout << "\t 04) Inserir/Excluir aresta" << endl;
+    cout << "\t 05) Inserir/Excluir Vértice" << endl;
+    cout << "\t 06) Retornar Grau do Grafo" << endl;
+    cout << "\t 07) Busca Grafo (profundidade/largura)" << endl;
+
+    cout << endl << endl;
+}
+
+
+
+int main()
+{   
+    cout << "\n#############################################\n";
+    cout << "Trabalho de Grafos - Grupo 2";
+    cout << "\n#############################################\n";
+    
+    cout << "Todo o desenvolvimento realizado nesse trabalho foi baseado para grafos simples,ou seja aqueles que não possuem multiarestas ou self-loop\n"; 
+    incluiMenu();
     return 0;
 }
